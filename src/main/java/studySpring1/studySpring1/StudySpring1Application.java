977d4dab1adb9a2b4b4116f6ddc0edaa15d344f7
11t518s <11t518s20161759@gmail.com>
2023-03-28 23:25:28 +0900

    first commit

---
+++ src/main/java/studySpring1/studySpring1/StudySpring1Application.java
@@ -0,0 +1,13 @@
+package studySpring1.studySpring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpring1Application.class, args);
+	}
+
+}
